154b356de3223040b58e62e9f3c0274652480523
UgoRastell <urastell@normandiewebschool.fr>
2022-12-09 14:47:03 +0100

    doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +13,11 @@ class HomeController extends AbstractController{
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(){
-        
-        return $this->render('home.html.twig');
+    public function homepage(ProductRepository $productRepository){
+        $products = $productRepository->findBy([],[],3);
+
+        return $this->render('home.html.twig',[
+        'products'=> $products
+        ]);
     }
 }
